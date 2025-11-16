a49cac5fb89c3a352dd5afcd888418ac245fbc9d
shezadalitechno <shezad@technoservesolutions.com>
2025-11-16 18:36:18 +0530

    Initial commit with project structure, including configuration files, environment setup, and basic application components. Added essential directories for resources, routes, and tests.

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\NaazimPanelProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
+];
